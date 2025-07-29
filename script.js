cc25d3d09d719a1597586f3f4b3e30f5a88e3fb9
Sameer Sangpal <66773934+SameerSangpal@users.noreply.github.com>
2025-07-29 06:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
-
+	const boldwords = document.querySelectorAll('strong');
+	boldwords.forEach(element => {
+		element.style.color = 'rgb(0, 128, 0)';
+	});
 }
-
 
 function return_normal() {
     //Write your code here
-
-    
+	const boldWords = document.querySelectorAll('strong');
+	boldWords.forEach(element => {
+		element.style.color = 'rgb(0, 0, 0)';
+	});  
 }
